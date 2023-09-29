4328bcbf4b79c4e2ccb5bac53ae5036e3d6918d2
mihai-bontea <mihaibontea@protonmail.com>
2023-09-29 22:57:55 +0300

    variable renaming

--- InfixToPostfix.py
+++ InfixToPostfix.py
@@ -12,8 +12,8 @@ class InfixToPostfixConverter:
   
     # Returns True if the precedence of operator is less than top of the stack, False otherwise
     @staticmethod
-    def not_greater(operator, stack): 
-        try: 
+    def not_greater(operator, stack):
+        try:
             precedence1 = OPERATOR_PRECEDENCE[operator]
             precedence2 = OPERATOR_PRECEDENCE[stack.top()] 
             return precedence1  < precedence2
@@ -41,11 +41,11 @@ class InfixToPostfixConverter:
                                 # Should be -1 at the end
 
 
-        for count, c in enumerate(exp): 
+        for count, char in enumerate(exp): 
             # Operand, add it to output 
-            if InfixToPostfixConverter.is_operand(c):
+            if InfixToPostfixConverter.is_operand(char):
                 last_connective = -1
-                output.append(c)
+                output.append(char)
                 
                 negation = False
                 oprn_oprt += 1
@@ -54,14 +54,13 @@ class InfixToPostfixConverter:
                     return False
               
             #'(', push it to stack
-            elif c  == '(': 
-                stack.push(c)
+            elif char  == '(': 
+                stack.push(char)
                 negation = False
-
                 open_parentheses += 1
   
             #')', pop and output from the stack until reaching '(' 
-            elif c == ')':
+            elif char == ')':
 
                 open_parentheses -= 1
                 if open_parentheses < 0:
@@ -87,14 +86,13 @@ class InfixToPostfixConverter:
                     stack.pop() 
   
             # Operator
-            elif InfixToPostfixConverter.is_operator(c):
+            elif InfixToPostfixConverter.is_operator(char):
                 last_connective = count
                 if count == len(exp) - 1:
                     print("String is not a WFF: expected WFF/Atom at index " + str(count))
                     return False
                 
-                if c != NEG:
-
+                if char != NEG:
                     oprn_oprt -= 1
                     if oprn_oprt < 0 or negation == True:
                         print("String is not a WFF: expected WFF/Atom at index " + str(count))
@@ -102,9 +100,9 @@ class InfixToPostfixConverter:
                 else:
                     negation = True
 
-                while not stack.is_empty() and InfixToPostfixConverter.not_greater(c, stack): 
+                while not stack.is_empty() and InfixToPostfixConverter.not_greater(char, stack): 
                     output.append(stack.pop()) 
-                stack.push(c)
+                stack.push(char)
             else:
                 print("String is not a WFF: illegal character at index " + str(count))
                 return False
@@ -127,4 +125,4 @@ class InfixToPostfixConverter:
         
         return ("".join(output))
     
-print(InfixToPostfixConverter.attempt_conversion("(A→B)→C"))
+print(InfixToPostfixConverter.attempt_conversion("(A→B)→C()"))
