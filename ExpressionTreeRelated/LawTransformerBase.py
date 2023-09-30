f12d7e5a9073fcc6b88fef939d6d5ce932feea77
mihai-bontea <mihaibontea@protonmail.com>
2023-09-30 21:09:27 +0300

    added base class for transformation laws

---
+++ ExpressionTreeRelated/LawTransformerBase.py
@@ -0,0 +1,12 @@
+from abc import abstractmethod
+from ExpressionTree import ExpressionTree
+
+class LawTransformerBase:
+    """Base class for laws which transform an expression tree."""
+    @staticmethod
+    @abstractmethod
+    def apply_law(expression_tree : ExpressionTree)->str:
+        """ Should return the summary of the changes, or "" if it couldn't be applied.
+            It shouldn't lead to any further changes when called again immediately after.
+        """
+        pass
